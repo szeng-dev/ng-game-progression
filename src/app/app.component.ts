a22712d029ede87f82d3c49acfe05bb36be73d78
Stephanie Zeng <stephanie.zeng@rangle.io>
2018-09-14 11:06:14 -0400

    Initial commit --- - Use tachyons - Add mockup page layout and basic styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  user: any;
+  pages: string[];
+
+  constructor() {
+    this.title = "Game Progression";
+    this.user = {
+      name: "Stephanie Zeng",
+      profileImage: "https://via.placeholder.com/50x50"
+    };
+    this.pages = ['Dashboard', 'Games'];
+  }
 }
