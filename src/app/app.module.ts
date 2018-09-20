f3e799aa9f31bd31f5ba89f24e3e70e5c93f2b66
Stephanie Zeng <stephanie.zeng@rangle.io>
2018-09-19 23:42:21 -0400

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+
+import { ApiService } from './services/api.service';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GamesComponent } from './games/games.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-
-import { ApiService } from './services/api.service';
+import { ProgressBarComponent } from './dashboard/progress-bar/progress-bar.component';
 
 const APP_ROUTES: Routes = [
   { path: '', component: DashboardComponent },
@@ -22,7 +24,8 @@ const APP_ROUTES: Routes = [
     AppComponent,
     DashboardComponent,
     GamesComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule,
