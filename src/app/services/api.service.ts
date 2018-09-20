7b68d076e47863e5c47dbffd7daedc2af4ad7075
Stephanie Zeng <stephanie.zeng@rangle.io>
2018-09-19 20:33:19 -0400

    Add api service to fetch data for layout

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+import { Profile } from '../models/profile.model';
+import { Language } from '../models/language.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  base_url: string = 'http://localhost:3004';
+
+  constructor(private http: HttpClient) { }
+
+  getProfile(): Observable<Profile> {
+    return this.http.get<Profile>(`${this.base_url}/profile`);
+  }
+
+  getLanguages(): Observable<Language[]> {
+    return this.http.get<Language[]>(`${this.base_url}/languages`);
+  }
+}
